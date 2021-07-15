d1c27f55d2bbc980ca43189d1111f2834fbd0010
ytyaru <yttry0@gmail.com>
2021-07-15 11:27:12 +0900

    Created a repository

---
+++ src/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -Ceu
+#---------------------------------------------------------------------------
+# Tera-cliをインストールする。
+# CreatedAt: 2021-07-15
+#---------------------------------------------------------------------------
+Run() {
+	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
+	cd "$HERE"
+	IsExistCmd() { type "$1" > /dev/null 2>&1; }
+	InstallRust() { curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh; }
+	InstallTera() { cargo install --git https://github.com/chevdor/tera-cli; }
+	IsExistCmd rustc || InstallRust
+	IsExistCmd tera || InstallTera
+	
+	cd ./0
+	tera -t README.tpl -o README.md README.tpl.toml
+}
+Run "$@"
